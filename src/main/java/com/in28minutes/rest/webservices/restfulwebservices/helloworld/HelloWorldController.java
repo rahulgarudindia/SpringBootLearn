6f6157944dfa9b3a580fa239172970e936b0ede8
rahul <'rahulgarudindia@gmail.com>
2022-10-06 19:42:15 +1100

    feature commit

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,34 @@
+
+package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.in28minutes.rest.webservices.restfulwebservices.helloworld.HelloWorldBean;
+
+@RestController
+public class HelloWorldController {
+	
+	@GetMapping(path = "/hello-world")
+	public String helloWorld() {
+		return "Hello World"; 
+	}
+	
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World"); 
+	}
+	
+	// Path Parameters
+	// /users/{id}/todos/{id}  => /users/2/todos/200
+	// /hello-world/path-variable/{name}
+	// /hello-world/path-variable/Ranga
+
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name)); 
+	}
+
+	
+	
+}
